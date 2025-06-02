6d3b3db29e4e3444e13d98ffdddbb4a4036b2f43
GuiSouza95 <guilhermesouzaa2018@gmail.com>
2025-06-02 10:24:49 +0100

    Primeiro commit

---
+++ controle_escolar/src/main/java/com/controle_escolar/controle_escolar/ControleEscolarApplication.java
@@ -0,0 +1,13 @@
+package com.controle_escolar.controle_escolar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ControleEscolarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ControleEscolarApplication.class, args);
+	}
+
+}
---
+++ controle_escolar/src/test/java/com/controle_escolar/controle_escolar/ControleEscolarApplicationTests.java
@@ -0,0 +1,13 @@
+package com.controle_escolar.controle_escolar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ControleEscolarApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
